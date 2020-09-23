cd5fae649b0914c00cf28489fce4bf8a8b41397a
Amos Rose <amosroseacct@gmail.com>
2020-09-23 16:29:43 -0600

    styling changes to better align skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -14,43 +14,43 @@ function Skills() {
   return (
     <div className="skills-container">
       <div className="skill-wrapper">
-        <img src={HTML} alt="HTML5 logo" width="100" />
+        <img src={HTML} alt="HTML5 logo" />
         <p>HTML</p>
       </div>
       <div className="skill-wrapper">
-        <img src={CSS} alt="CSS logo" width="100" />
+        <img src={CSS} alt="CSS logo" />
         <p>CSS</p>
       </div>
       <div className="skill-wrapper">
-        <img src={JavaScript} alt="JavaScript logo" width="100" />
+        <img src={JavaScript} alt="JavaScript logo" />
         <p>JavaScript</p>
       </div>
       <div className="skill-wrapper">
-        <img src={React_logo} alt="React logo" width="100" />
+        <img src={React_logo} alt="React logo" />
         <p>React</p>
       </div>
       <div className="skill-wrapper">
-        <img src={Redux} alt="Redux logo" width="100" />
+        <img src={Redux} alt="Redux logo" />
         <p>Redux</p>
       </div>
       <div className="skill-wrapper">
-        <img src={React_Native} alt="React Native logo" width="100" />
+        <img src={React_Native} alt="React Native logo" />
         <p>React Native</p>
       </div>
       <div className="skill-wrapper">
-        <img src={Sass} alt="Sass logo" width="100" />
+        <img src={Sass} alt="Sass logo" />
         <p>Sass</p>
       </div>
       <div className="skill-wrapper">
-        <img src={Less} alt="Less logo" width="100" />
+        <img src={Less} alt="Less logo" />
         <p>Less</p>
       </div>
       <div className="skill-wrapper">
-        <img src={SQL} alt="SQL logo" width="100" />
+        <img src={SQL} alt="SQL logo" />
         <p>SQL</p>
       </div>
       <div className="skill-wrapper">
-        <img src={PostgreSQL} alt="PostgreSQL logo" width="100" />
+        <img src={PostgreSQL} alt="PostgreSQL logo" />
         <p>PostgreSQL</p>
       </div>
     </div>
